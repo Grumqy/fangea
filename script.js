c29eee73bd89aea444cffec087d69260786853d3
Grumqy <152092935+Grumqy@users.noreply.github.com>
2023-11-27 16:23:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,36 +4,31 @@ document.addEventListener('DOMContentLoaded', function () {
     let isDragging = false;
     let startX, startY, translateX = 0, translateY = 0, prevX, prevY, scale = 1;
 
-    function toggleDragging() {
-        isDragging = !isDragging;
-    }
-
     document.addEventListener('mousedown', (e) => {
         if (e.button !== 0) return; // Ignoruj inne przyciski niż lewy
-        if (!isDragging) {
-            toggleDragging();
-            startX = e.clientX - map.offsetLeft;
-            startY = e.clientY - map.offsetTop;
-            prevX = e.clientX;
-            prevY = e.clientY;
-        }
+        isDragging = true;
+        startX = e.clientX - map.offsetLeft;
+        startY = e.clientY - map.offsetTop;
+        prevX = e.clientX;
+        prevY = e.clientY;
     });
 
     document.addEventListener('mouseup', () => {
         if (isDragging) {
-            toggleDragging();
+            isDragging = false;
         }
     });
 
     document.addEventListener('mousemove', (e) => {
         if (!isDragging) return;
 
-        const offsetX = e.clientX - prevX;
-        const offsetY = e.clientY - prevY;
+        const offsetX = (e.clientX - prevX) / scale;
+        const offsetY = (e.clientY - prevY) / scale;
 
         translateX += offsetX;
         translateY += offsetY;
 
+        map.style.transition = 'none'; // Wyłącz efekt przejścia podczas płynnego poruszania
         map.style.transform = `scale(${scale}) translate(${translateX}px, ${translateY}px)`;
 
         prevX = e.clientX;
@@ -51,12 +46,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Ogranicz przybliżenie i oddalenie
-        if (scale > 1000) {
-            scale = 1000;
+        if (scale > 20) {
+            scale = 20;
         } else if (scale < 0.2) {
             scale = 0.2;
         }
 
+        map.style.transition = 'transform 0.2s ease-out'; // Włącz efekt przejścia podczas płynnego scrollowania
         map.style.transform = `scale(${scale}) translate(${translateX}px, ${translateY}px)`;
 
         e.preventDefault(); // Zapobiega domyślnemu zachowaniu zooma przeglądarki
@@ -64,7 +60,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.addEventListener('mouseleave', () => {
         if (isDragging) {
-            toggleDragging();
+            isDragging = false;
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        if (isDragging) {
+            isDragging = false;
+            map.style.transition = 'transform 0.2s ease-out'; // Dodaj efekt przejścia po odkliknięciu przycisku myszy
+            map.style.transform = `scale(${scale}) translate(${translateX}px, ${translateY}px)`;
         }
     });
 });
