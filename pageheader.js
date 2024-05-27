82be0ddfd9d811f2edc3dbfbf60933afc8af2707
Grumqy <152092935+Grumqy@users.noreply.github.com>
2024-05-27 10:16:14 +0200

    Update pageheader.js

--- pageheader.js
+++ pageheader.js
@@ -15,7 +15,7 @@ document.querySelector("header#pageheader").innerHTML = `
             <section id="pageheader-menu">
                 <ul>
                 <li><a href="https://grumqy.github.io/fangea/mapa/"><i class="fa-solid fa-map"></i><span>Mapa</span></a></li>
-                <li><a href="https://grumqy.github.io/fangea/radio/stacje_radiowe/"><i class="fa-solid fa-radio"></i><span>Radio</span></a></li>
+                <li><a href="https://grumqy.github.io/fangea/radio/"><i class="fa-solid fa-radio"></i><span>Radio</span></a></li>
                  </ul>
             </section>
         </nav>`;
