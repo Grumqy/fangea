73f33fc34d4775fefbdff90fb3faac16cbe8194d
Grumqy <152092935+Grumqy@users.noreply.github.com>
2024-05-30 23:57:01 +0200

    Update article.js

--- article.js
+++ article.js
@@ -13,7 +13,7 @@ Array.from(headings).forEach((element) => {
   let section = {
     name: element.textContent,
     position: getSectionPosition(element),
-    element: element, // Assigning the element property
+    element: element,
   };
   sections.push(section);
 });
@@ -29,12 +29,93 @@ sections.forEach((section) => {
     });
   });
 });
-let images = document.querySelectorAll('main article img:not(.icon)');
 
-images.forEach(image => {
-    let anchor = document.createElement('a');
-    anchor.href = image.src;
-    anchor.target = "_blank";
-    anchor.appendChild(image.cloneNode(true));
-    image.parentNode.replaceChild(anchor, image);
-});
+let pictures = Array.from(document.querySelectorAll("main article table.infobox img:not(.icon)")).map(img => ({
+  src: img.src,
+  description: img.dataset.description
+})).concat(Array.from(document.querySelectorAll("main article figure")).map(figure => {
+  let img = figure.querySelector("img");
+  let figcaption = figure.querySelector("figcaption");
+  return {
+      src: img ? img.src : "",
+      description: figcaption ? figcaption.innerHTML.trim() : ""
+  };
+}));
+
+let PicturesElements = Array.from(document.querySelectorAll("main article table.infobox img:not(.icon)")).concat(Array.from(document.querySelectorAll("main article figure")));
+
+PicturesElements.forEach((element,index) =>{
+element.addEventListener("click", () => selectGalleryItem(index));
+})
+
+console.log(PicturesElements);
+
+console.log(pictures);
+
+
+let modalContainer = document.createElement("div");
+modalContainer.id = "modal-container";
+document.body.appendChild(modalContainer);
+
+modalContainer.innerHTML = `
+<div class="modal">
+<i class="fa-solid fa-xmark"></i>
+<div id="modal-gallery-main">
+    <ul id="modal-gallery-list">
+    </ul>
+</div>
+<div id="modal-gallery-nav">
+    <ul id="modal-gallery-nav-list">
+    </ul>
+    </div>
+  </div>
+<div id="modal-overlay"></div>
+`;  
+
+document.querySelector(".modal i.fa-solid").addEventListener("click", hideModal);
+document.getElementById("modal-overlay").addEventListener("click", hideModal);
+
+pictures.forEach((picture, index) => {
+let galleryItem = document.createElement("li");
+galleryItem.classList.add("modal-gallery-item");
+galleryItem.dataset.index = index;
+galleryItem.innerHTML = `<div class="modal-gallery-item-image"><img src="${picture.src}"></div>
+<div class="modal-gallery-item-description">${picture.description}</div>`;
+
+document.getElementById("modal-gallery-list").appendChild(galleryItem);
+
+let navItem = document.createElement("li");
+navItem.classList.add("modal-gallery-nav-item");
+navItem.dataset.index = index;
+navItem.innerHTML = `<img src="${picture.src}">`;
+navItem.addEventListener("click", () => selectGalleryItem(index));
+document.getElementById("modal-gallery-nav-list").appendChild(navItem);
+
+})
+
+let galleryItems = Array.from(document.querySelectorAll("ul#modal-gallery-list li.modal-gallery-item"));
+let galleryNavItems = Array.from(document.querySelectorAll("ul#modal-gallery-nav-list li.modal-gallery-nav-item"));
+
+function selectGalleryItem(index){
+  galleryItems.forEach(element => {
+  element.classList.remove("active");
+  galleryItems[index].classList.add("active");
+  })
+  galleryNavItems.forEach(element => {
+  element.classList.remove("active");
+  galleryNavItems[index].classList.add("active");
+  })
+  showModal();
+  }
+
+function showModal(){
+document.getElementById("modal-container").classList.add("active");
+}
+function hideModal(){
+
+document.getElementById("modal-container").classList.add("hidden");
+setTimeout(function(){
+  document.getElementById("modal-container").classList.remove("active");
+  document.getElementById("modal-container").classList.remove("hidden");
+}, 250)
+}
